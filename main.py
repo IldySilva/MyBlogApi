4e7cac1643b59a04d8b07828244c1abaf6d521df
IldySilva <ildeberto34@gmail.com>
2021-11-28 13:37:51 +0100

    setting postgres

--- main.py
+++ main.py
@@ -1,31 +1,29 @@
 from fastapi import FastAPI
-import mysql.connector 
-import mysql
+from fastapi.encoders import jsonable_encoder
+import psycopg2
 from model.loginModel import LoginModel
+
+from fastapi.responses import JSONResponse
 import json
 
 from model.userModel import UserModel
+app = FastAPI()
 
 
-database=mysql.connector.connect(host="127.0.0.1",user="root",database="myblog")
-cursor=database.cursor()
-app=FastAPI()
-
+database = psycopg2.connect(
+    host='localhost', dbname="myblog", user="postgres", password="root")
+cursor = database.cursor()
 
 
 @app.post("/login")
 def makeLogin(loginModel: LoginModel):
-    cursor.execute("SELECT * FROM usuarios WHERE phone = %s AND pwd = %s", (loginModel.phone, loginModel.password)) 
-    data= cursor.fetchall()
-    if data :
-       user=UserModel.fromJson(data[0])
-  
-       return user.toJson()
-      
+    cursor.execute("SELECT * FROM person WHERE phone = %s AND pwd = %s",
+                   (loginModel.phone, loginModel.password))
+    data = cursor.fetchall()
+    if data:
+        user = UserModel.fromJson(data[0])
+
+        return JSONResponse(jsonable_encoder(user))
+
     else:
         return {"Wrong Username or password"}
-
-
-
-
-
